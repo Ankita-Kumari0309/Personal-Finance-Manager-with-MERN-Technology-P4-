a262ab5699d5a0f65716f0194a196e81999b027b
Ankita Kumari <163445458+Ankita-Kumari0309@users.noreply.github.com>
2025-03-02 17:11:55 +0530

    Add files via upload

---
+++ Routers/userRouter.js
@@ -0,0 +1,25 @@
+import { validationResult } from "express-validator";
+import User from "../models/User.js";
+
+export const registerControllers = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ success: false, errors: errors.array() });
+  }
+
+  const { username, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    user = new User({ username, email, password });
+    await user.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
